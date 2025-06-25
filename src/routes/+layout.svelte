ac1ded9193aa7ab6bf90fe4a8baaefe7cb352f76
albetnov <ryxinfrvr@gmail.com>
2025-06-25 09:10:47 +0700

    refactor: switch to @sveltejs/adapter-static and update dependencies

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,13 @@
 <script>
-    import gsap from 'gsap';
-    import { ScrollTrigger } from 'gsap/ScrollTrigger';
-    import { SplitText } from 'gsap/SplitText';
-	
-	import '../app.css';
-	
-	let { children } = $props();
+    import "../app.css";
+    import gsap from "gsap";
+    import { ScrollTrigger } from "gsap/ScrollTrigger";
+    import { SplitText } from "gsap/SplitText";
+    import {CSSPlugin} from "gsap/CSSPlugin"
 
-	gsap.registerPlugin(ScrollTrigger, SplitText);
+    gsap.registerPlugin(ScrollTrigger, SplitText, CSSPlugin);
+
+    let { children } = $props();
 </script>
 
 {@render children()}
